f2566241cb2d60cc0b6bd696b38e739b704e922f
vanessa082 <wahvanessa22@gmail.com>
2024-10-01 14:44:19 +0100

    added a cors origin

---
+++ src/middlewares/auth.validator.ts
@@ -0,0 +1,15 @@
+// import Joi from "joi";
+
+// const registerSchema = Joi.object({
+//   firstName: Joi.string().min(3).max(30)
+// })
+
+// class AuthValidator{
+//   constructor() {
+
+//   };
+
+// validateCreateAccount (req, res, next ){
+
+// }
+// }
